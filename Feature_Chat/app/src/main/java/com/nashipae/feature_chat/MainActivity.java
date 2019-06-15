94521a9232401e2c6299cf6b647e1f51228a4ff6
Nashipae <nashkiki@gmail.com>
2019-06-16 02:20:34 +0300

    added an onClickListener to 'sendButton'.

--- Feature_Chat/app/src/main/java/com/nashipae/feature_chat/MainActivity.java
+++ Feature_Chat/app/src/main/java/com/nashipae/feature_chat/MainActivity.java
@@ -3,17 +3,25 @@ package com.nashipae.feature_chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mSendButton;
+    private Button mSendButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSendButton = (Button) findViewById(R.id.sendButton1);
+        mSendButton1 = (Button) findViewById(R.id.sendButton1);
+        mSendButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Message Sent", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
