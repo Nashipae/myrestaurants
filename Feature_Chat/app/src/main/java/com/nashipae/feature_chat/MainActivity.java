edb500e36149ff2b54dec214210ee1625dc2383c
Nashipae <nashkiki@gmail.com>
2019-06-16 09:52:57 +0300

    added method for gathering data from EditText.

--- Feature_Chat/app/src/main/java/com/nashipae/feature_chat/MainActivity.java
+++ Feature_Chat/app/src/main/java/com/nashipae/feature_chat/MainActivity.java
@@ -2,14 +2,19 @@ package com.nashipae.feature_chat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mSendButton;
+    private EditText mChatEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Message Sent", Toast.LENGTH_LONG).show();
+
+                String chat = mChatEditText.getText().toString();
+                Log.d(TAG, chat);
+
+//                Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+//                startActivity(intent);
             }
         });
     }
