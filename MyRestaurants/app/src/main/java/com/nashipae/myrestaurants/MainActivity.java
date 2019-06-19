e141ceb8f2029a329f90a708e8051642200b4947
Nashipae <nashkiki@gmail.com>
2019-06-19 19:00:30 +0300

    changed the typeface to ostrich font.

--- MyRestaurants/app/src/main/java/com/nashipae/myrestaurants/MainActivity.java
+++ MyRestaurants/app/src/main/java/com/nashipae/myrestaurants/MainActivity.java
@@ -2,13 +2,54 @@ package com.nashipae.myrestaurants;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+
+
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+//                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+//                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+//                startActivity(intent);
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+                }
+
+
+
+        });
+
+
+
+
     }
 }
