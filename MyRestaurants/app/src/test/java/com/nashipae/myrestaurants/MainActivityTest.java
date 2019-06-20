990ace1bd19c524692427b18586b39bedfde44c5
Nashipae <nashkiki@gmail.com>
2019-06-20 12:11:43 +0300

    implemented listview.

--- MyRestaurants/app/src/test/java/com/nashipae/myrestaurants/MainActivityTest.java
+++ MyRestaurants/app/src/test/java/com/nashipae/myrestaurants/MainActivityTest.java
@@ -2,16 +2,17 @@ package com.nashipae.myrestaurants;
 
 import org.junit.Before;
 import org.junit.runner.RunWith;
-//import org.robolectric.Robolectric;
-//import org.robolectric.RobolectricTestRunner;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
 
-//@RunWith(RobolectricTestRunner.class)
-//public class MainActivityTest {
-//        private MainActivity activity;
-//
-//        @Before
-//        public void setup(){
-//            activity = Robolectric.setupActivity(MainActivity.class);
-//        }
-//
-//}
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+        private MainActivity activity;
+
+        @Before
+        public void setup(){
+            activity = Robolectric.setupActivity(MainActivity.class);
+        }
+
+}
