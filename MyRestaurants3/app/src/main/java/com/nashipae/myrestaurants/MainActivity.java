83f20549aa2fd2a4b53b18a81de01c9d42ee104a
Nashipae <nashkiki@gmail.com>
2019-08-14 14:51:02 +0300

    added a toast on find restaurants button.

--- MyRestaurants3/app/src/main/java/com/nashipae/myrestaurants/MainActivity.java
+++ MyRestaurants3/app/src/main/java/com/nashipae/myrestaurants/MainActivity.java
@@ -3,12 +3,24 @@ package com.nashipae.myrestaurants;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
