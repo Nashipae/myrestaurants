82ff741107c2302b194d74c5047d65a2af16236c
Nashipae <nashkiki@gmail.com>
2019-08-14 15:02:58 +0300

    added an intent to main activity.

--- MyRestaurants3/app/src/main/java/com/nashipae/myrestaurants/MainActivity.java
+++ MyRestaurants3/app/src/main/java/com/nashipae/myrestaurants/MainActivity.java
@@ -2,6 +2,7 @@ package com.nashipae.myrestaurants;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
             }
         });
     }
