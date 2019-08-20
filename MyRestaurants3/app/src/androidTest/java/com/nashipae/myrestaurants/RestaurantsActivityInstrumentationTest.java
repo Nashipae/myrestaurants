afc19334336178d0b4ae72bc30875f144ce7d27a
Nashipae <nashkiki@gmail.com>
2019-08-20 14:46:04 +0300

    added restaurantactivity tests.

---
+++ MyRestaurants3/app/src/androidTest/java/com/nashipae/myrestaurants/RestaurantsActivityInstrumentationTest.java
@@ -0,0 +1,34 @@
+package com.nashipae.myrestaurants;
+
+import androidx.test.rule;
+import android.view.View;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsAnything.anything;
+
+public class RestaurantsActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<RestaurantsActivity> activityActivityTestRule = new ActivityTestRule<>(RestaurantsActivity.class);
+
+
+    @Test
+    public void listItemClickDisplaysToastWithCorrectRestaurant() {
+        View activityDecorView = activityTestRule.getActivity().getWindow().getDecorView();
+        String restaurantName = "Mi Mero Mole";
+        onData(anything())
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .perform(click());
+        onView(withText(restaurantName)).inRoot(withDecorView(not(activityDecorView)))
+                .check(matches(withText(restaurantName)));
+    }
+}
---
+++ MyRestaurants3/app/src/androidTest/java/com/nashipae/myrestaurants/RestaurantsActivityTest.java
@@ -0,0 +1,35 @@
+package com.nashipae.myrestaurants;
+
+import android.os.Build;
+import android.widget.ListView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class RestaurantsActivityTest {
+    private RestaurantsActivity activity;
+    private ListView mRestaurantListView;
+
+    @Before
+    public void setUp(){
+        activity = Robolectric.setupActivity(RestaurantsActivity.class);
+        mRestaurantListView = (ListView) activity.findViewById(R.id.listView);
+
+    }
+
+    @Test
+    public void restaurantListViewPopulates() {
+        assertNotNull(mRestaurantListView.getAdapter());
+        assertEquals(mRestaurantListView.getAdapter().getCount(), 15);
+    }
+}
