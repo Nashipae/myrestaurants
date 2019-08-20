ac28ef0ac79fecd35b295f40ee73af0d6ccf45d1
Nashipae <nashkiki@gmail.com>
2019-08-20 13:42:14 +0300

    added restaurant listview.

--- MyRestaurants3/app/src/main/java/com/nashipae/myrestaurants/MainActivity.java
+++ MyRestaurants3/app/src/main/java/com/nashipae/myrestaurants/MainActivity.java
@@ -1,27 +1,33 @@
 package com.nashipae.myrestaurants;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantButton;
     private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantButton = (Button) findViewById(R.id.findRestaurantsButton);
+//        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+//        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+//        mAppNameTextView.setTypeface(ostrichFont);
 
         mFindRestaurantButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +40,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
