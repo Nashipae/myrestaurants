941e747d759da61e61c76940b7f7e6abaaa0af54
Nashipae <nashkiki@gmail.com>
2019-08-29 11:33:03 +0300

    added myRestaurantsArrayAdapter.

--- MyRestaurants3/app/src/main/java/com/nashipae/myrestaurants/MainActivity.java
+++ MyRestaurants3/app/src/main/java/com/nashipae/myrestaurants/MainActivity.java
@@ -2,44 +2,38 @@ package com.nashipae.myrestaurants;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private Button mFindRestaurantButton;
-    private EditText mLocationEditText;
-    private TextView mAppNameTextView;
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantButton = (Button) findViewById(R.id.findRestaurantsButton);
-//        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
 //        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
 //        mAppNameTextView.setTypeface(ostrichFont);
 
-        mFindRestaurantButton.setOnClickListener(new View.OnClickListener() {
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 String location = mLocationEditText.getText().toString();
-                Log.d(TAG, location);
-                Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
                 intent.putExtra("location", location);
                 startActivity(intent);
             }
         });
-
     }
 }
