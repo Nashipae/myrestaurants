5671b8b7e41fced5cdd161980e1c27cf6c4143f5
Nashipae <nashkiki@gmail.com>
2019-10-01 14:48:18 +0300

    add Recycler view Restaurant

--- app/src/main/java/com/nashipae/myrestaurants/ui/RestaurantsActivity.java
+++ app/src/main/java/com/nashipae/myrestaurants/ui/RestaurantsActivity.java
@@ -2,14 +2,13 @@ package com.nashipae.myrestaurants.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.nashipae.myrestaurants.R;
+import com.nashipae.myrestaurants.adapters.RestaurantListAdapter;
 import com.nashipae.myrestaurants.models.Restaurant;
 import com.nashipae.myrestaurants.services.YelpService;
 
@@ -27,8 +26,8 @@ import okhttp3.Response;
 public class RestaurantsActivity extends AppCompatActivity {
     private static final String TAG = RestaurantsActivity.class.getSimpleName();
 
-    @BindView(R.id.locationTextView) TextView mLocationTextView;
-    @BindView(R.id.listView) ListView mListView;
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    private RestaurantListAdapter mAdapter;
 
     public ArrayList<Restaurant> restaurants = new ArrayList<>();
 
@@ -41,7 +40,7 @@ public class RestaurantsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
+
         getRestaurants(location);
 
     }
@@ -63,23 +62,13 @@ public class RestaurantsActivity extends AppCompatActivity {
                     RestaurantsActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            String[] restaurantNames = new String[restaurants.size()];
-                            for (int i = 0; i < restaurantNames.length; i++) {
-                                restaurantNames[i] = restaurants.get(i).getName();
-                            }
-
-                            ArrayAdapter adapter = new ArrayAdapter(RestaurantsActivity.this,android.R.layout.simple_list_item_1,restaurantNames);
-                            mListView.setAdapter(adapter);
-
-                            for (Restaurant restaurant : restaurants) {
-                                Log.d(TAG, "Name: " + restaurant.getName());
-                                Log.d(TAG, "Phone: " + restaurant.getPhone());
-                                Log.d(TAG, "Website: " + restaurant.getWebsite());
-                                Log.d(TAG, "Image url: " + restaurant.getImageUrl());
-                                Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
-                                Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
-                                Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
-                            }
+                            mAdapter = new RestaurantListAdapter(getApplicationContext(), restaurants);
+                            mRecyclerView.setAdapter(mAdapter);
+                            RecyclerView.LayoutManager layoutManager =
+                                    new LinearLayoutManager(RestaurantsActivity.this);
+                            mRecyclerView.setLayoutManager(layoutManager);
+                            mRecyclerView.setHasFixedSize(true);
+                        
                         }
                         });
                     }
